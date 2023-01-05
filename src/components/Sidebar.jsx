5abf897bc041f55889166a9c203fd3e8ec68b626
MirthaCalafate <mirtha.aleja@gmail.com>
2023-01-05 14:31:28 -0300

    actualizaciones

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
 import '../styles/sidebar.css';
+import ButtonList from '../components/ButtonList';
+import data from '../data/data';
+import { useState } from 'react';
 
 export default function Sidebar() {
+    
+    const allCategories = [
+		'Todas',
+		...new Set(data.map(article => article.category)),
+	];
+
+    const [categories, setCategories] = useState(allCategories);
+	const [articles, setArticles] = useState(data);
+
+    const filterCategory = (category) => {
+		if (category === 'Todas'){
+			setArticles(data)
+			return
+		}
+
+	const filteredData = data.filter(article => article.category === category);
+		setArticles(filteredData)
+	}
+
   return (
     <div className="sidebar">
       <div className="sidebarItem">
-        <span className="sidebarTitle">ABOUT ME</span>
-        <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
-          alt=""
-        />
+        <h5>SIDEBAR</h5>
         <p>
           Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
           amet ex esse.Sunt eu ut nostrud id quis proident.
@@ -17,38 +35,7 @@ export default function Sidebar() {
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIAS</span>
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <Link className="link" to="/blog?categories=redes">
-              Redes
-            </Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Music">
-              Music
-            </Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Sport">
-              Sport
-            </Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Style">
-              Style
-            </Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Tech">
-              Tech
-            </Link>
-          </li>
-          <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Cinema">
-              Cinema
-            </Link>
-          </li>
-        </ul>
+        <ButtonList categories={categories} filterCategory={filterCategory}/>
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">Seguinos</span>
